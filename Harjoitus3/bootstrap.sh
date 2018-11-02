bbe766bd2caeb93f5a1c46ebe8b659bcb8551220
MaijaP <maija.prove@gmail.com>
2018-11-02 14:37:30 +0200

    perjantai 4

--- Harjoitus3/bootstrap.sh
+++ Harjoitus3/bootstrap.sh
@@ -1,33 +1,15 @@
 sudo apt-get update
-sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
-sudo docker run hello-world
-
 
 sudo apt-get -y install git
 sudo mkdir /var/tmp/demo
 cd /var/tmp/demo
 sudo git clone https://github.com/docker-training/webapp.git
-
-sudo chmod u+x DevOps-Lab/Harjoitus\ 3/Vagrant/test.sh
-sudo DevOps-Lab/Harjoitus\ 3/Vagrant/test.sh
-
-
-# Docker imagen koonti Dockerfilestä
+cd webapp
 sudo docker build -t testwebapp .
-
-# Docker imagen käynnistys
 sudo docker run -d -p 80:5000 testwebapp
-
-
